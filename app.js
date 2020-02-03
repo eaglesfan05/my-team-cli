a224ac18cc379237cd37b02cca0e0e96ecd6dec5
Khalil <khalilhall1221@gmail.com>
2020-02-03 08:41:29 -0500

    set up engineer function

--- app.js
+++ app.js
@@ -19,16 +19,11 @@ function getRole(){
         return console.log("title is: " + res.title);
         title = res.title;
         //is catching manager as title but not passing into function//
-        buildTeam(res.title);
+    buildTeam(res.title);
     }).catch(err=>{
         console.log(err + "try again.")
         process.exit(1);
-    })
-    
-    
-    
-    
-    
+    })    
     // console.log(title);
 }getRole();
 
@@ -75,9 +70,36 @@ function newIntern() {
             name: "school",
             message: "What school did you go to?"
         }
-    ])
+    ]).then(res =>{
+        return console.log("Name: " + res.name,'\n',"Email: " + res.email, "\n", "School: " + res.school)
+    }).catch(err =>{
+        console.log(err + "try again.")
+        process.exit(1);
+    })
+    getRole()
 }
 
+
+function newEngineer(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your name."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email."
+        },
+        
+    ]).then(res =>{
+        return console.log("Name: " + res.name,'\n',"Email: " + res.email)
+    }).catch(err =>{
+        console.log(err + "try again.")
+        process.exit(1);
+    })
+}
 //function that handles which employee object to build//
 function buildTeam(res){
     
